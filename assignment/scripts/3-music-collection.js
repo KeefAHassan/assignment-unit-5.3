fbc3b5a92d700fdcba2e49ce44d2af995891eec7
keef hassan <keefhassan4@gmail.comgit config --global user.email keefhassan4@gmail.coome>
2024-01-12 02:22:44 -0600

    Halfway done with My album collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,12 +2,40 @@ console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
 
+let myCollection = [];
+
+function addToCollection (collection, title, artist, yearPublished) {
+  // your code here...
+  let myrecord = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+
+  };
+
+  collection.push(myrecord);
+
+  return myrecord
+}
+addToCollection(myCollection,"good girl gone bad", "Rihanna",2007);
+addToCollection(myCollection,"a girl like me", "Rihanna",2006);
+addToCollection(myCollection,"1989","Taylor Swift",2014);
+addToCollection(myCollection,"Red","Taylor Swift",2012);
+addToCollection(myCollection,"SO SICK"," NE-YO",2006);
+addToCollection(myCollection,"MISS INDEPENDENT"," NE-YO",2008);
+
+
+console.log(myCollection)
 
 
 
-
-
-
+function showCollection (collection){
+for (let index = 0; index < collection.length; index++) {
+  const element = collection[index];
+  console.log(`${element.title} by ${element.artist}, published in ${element.yearPublished}`)
+}
+}
+showCollection(myCollection);
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
